8e1648262a29aa09aca3539947126b00270b4969
Evinya <51764441+Evinya@users.noreply.github.com>
2021-05-13 17:21:24 -0500

    Adding election audit code

--- PyPoll.py
+++ PyPoll.py
@@ -9,6 +9,16 @@ import os
 file_open_path = 'Resources/election_results.csv'
 file_save_path = os.path.join("Analysis", "election_analysis.txt")
 
+# Initialize variables
+total_votes = 0
+candidate_options = []
+candidate_votes = {}
+
+# Winning Candidate and Winning Count Tracker
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
+
 # open election_results.csv
 with open(file_open_path) as election_data:
 
@@ -18,8 +28,45 @@ with open(file_open_path) as election_data:
     # Read & print header row
     headers = next(file_reader)
     print(headers)
-    #for row in file_reader:
-    #    print(row)
+
+    # Get total number of votes (total_votes)
+    for row in file_reader:
+        total_votes += 1
+
+        # Get list candidates who got votes 
+        candidate_name = row[2]
+        if candidate_name not in candidate_options:
+
+            # Add candidate name to list of candidates
+            candidate_options.append(candidate_name)
+
+            # Begin tracking candidate vote count
+            candidate_votes[candidate_name] = 0
+
+        # Tally candidate votes
+        candidate_votes[candidate_name] +=1
+
+# Determine vote totals & percentage of candidates
+for candidate_name in candidate_options:
+    votes = candidate_votes[candidate_name]
+    vote_percentage = (float(votes)/float(total_votes))*100
+
+    print(f"{candidate_name}: {vote_percentage}%, {votes}")
+
+    # Determine highest vote & winner
+    if (votes > winning_count) and (vote_percentage > winning_percentage):
+        winning_count = votes
+        winning_percentage = vote_percentage
+        winning_candidate = candidate_name
+
+# Summarize the winner
+winning_candidate_summary = (
+    f"-------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"-------------------------\n")
+print(winning_candidate_summary)
 
 
 # save output to election_analysis.txt
